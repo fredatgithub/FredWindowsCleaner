ae1383c393eca459ed78ce8e821550313404af4f
fred <domaine75@gmail.com>
2024-08-25 14:01:20 +0200

    add roadmap

--- FredWindowsCleaner/FormMain.Designer.cs
+++ FredWindowsCleaner/FormMain.Designer.cs
@@ -37,6 +37,7 @@
       this.ClientSize = new System.Drawing.Size(800, 450);
       this.Name = "FormMain";
       this.Text = "Fred Windows Cleaner";
+      this.Load += new System.EventHandler(this.FormMain_Load);
       this.ResumeLayout(false);
 
     }
--- FredWindowsCleaner/FormMain.cs
+++ FredWindowsCleaner/FormMain.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FredWindowsCleaner
@@ -15,6 +8,26 @@ namespace FredWindowsCleaner
     public FormMain()
     {
       InitializeComponent();
+    }
+
+    private void FormMain_Load(object sender, EventArgs e)
+    {
+      // spec to be cleaned off
+      // 1. éléments confidentiels à risque
+      // 11. Internet Explorer cache
+      // C:\Users\user1\AppData\Local\Microsoft\Windows\INetCache\IE\
+      // C:\Users\user1\AppData\LocalLow\Microsoft\CryptnetUrlCache\Content\*.*
+      // 12. Mozilla Firefox cache
+      // 13. Chronium cache
+      // 14. Mozilla Firefox historique
+      // 15. Google Chrome historique
+      // 16. Internet Explorer cookies
+      // 17. Mozilla Firefox cookies
+      // 2. Nettoyage de l'espace disque
+      // 3. Entrées résiduelles du registre
+      // 4. Racourcis non valides
+      // 5. Vider la corbeille
+
     }
   }
 }
